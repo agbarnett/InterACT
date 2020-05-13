cb37f22478affae2003b774aeec3fe199f5f464e
Adrian Barnett <a.barnett@qut.edu.au>
2020-05-14 09:57:42 +1000

    Add files via upload

---
+++ 99_functions.R
@@ -0,0 +1,39 @@
+# 99_functions.R
+# helpful functions for InterACT
+# May 2020
+
+# rename for SPICT variables
+nice.rename.spict = function(x){
+y = case_when(
+  x == 'spict_unplan_hosp' ~ 'Unplanned hospital',
+  x == 'spict_perform_status' ~ 'Performance poor',
+  x == 'spict_care_others' ~ 'Depends on others',
+  x == 'spict_weight_loss' ~ 'Weight loss',
+  x == 'spict_persist_sympt' ~ 'Persistent symptoms',
+  x == 'spict_care_focus' ~ 'Palliative care request'
+)
+return(y)  
+}
+
+# rename for CRISTAL variables
+nice.rename.cristal = function(x){
+  y = case_when(
+    x == 'cristal_admit_ed' ~ 'Admitted via ED',
+    x == 'cristal_admit_source' ~ 'From nursing home',
+    x == 'age' ~ 'Age',
+    x == 'cristal_previous_admit' ~ 'Previous hospitalisation',
+    x == 'cristal_icu' ~ 'ICU admission',
+    x == 'cristal_cfs_score' ~ 'Frailty score >= 6',
+    x == "cristal_cancer" ~ 'Advanced cancer',
+    x == "cristal_proteinuria"  ~ 'Proteinuria',
+    x == "cristal_ckd"  ~ 'Chronic kidney disease',
+    x == "cristal_ecg" ~ 'Abnormal ECG',
+    x == "cristal_ami" ~ 'Acute myocardial infarction',
+    x == "cristal_chf"  ~ 'Chronic heart failure',          
+    x ==  "cristal_copd" ~ 'COPD',
+    x == "cristal_stroke" ~ 'Stroke',
+    x == "cristal_cognitive" ~ 'Cognitive impairment',     
+    x ==  "cristal_liver" ~ 'Liver disease'
+  )
+  return(y)  
+}
